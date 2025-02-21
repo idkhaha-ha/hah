51f545058fb5e742e486ade8f64ac1e9de7f91e3
idkhaha-ha <idkhaaaaha@gmail.com>
2025-02-21 20:14:07 +0800

    第一次上傳網站檔案

---
+++ page2(scripts).js
@@ -0,0 +1,11 @@
+const slides = document.querySelectorAll('.slide');
+let currentIndex = 0;
+
+function showNextSlide() {
+  
+    slides[currentIndex].classList.remove('active');
+  
+    currentIndex = (currentIndex + 1) % slides.length;
+    
+    slides[currentIndex].classList.add('active');
+}
